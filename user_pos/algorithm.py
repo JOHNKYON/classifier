6a7e565d5f0e5a8a405e365b41984779215f0ef7
JOHNKYON <zhaofengbo1321@163.com>
2015-11-07 15:21:23 +0800

    Add new test

--- user_pos/algorithm.py
+++ user_pos/algorithm.py
@@ -9,6 +9,7 @@ import data
 import numpy
 import codecs
 import json
+import random
 
 from sklearn import tree
 from sklearn.ensemble import RandomForestClassifier
@@ -105,9 +106,9 @@ def run_decision_tree():
 
 
 def run_random_forest():
-    log_conf.logger.info("Random forest startung...")
+    # log_conf.logger.info("Random forest startung...")
 
-    log_conf.logger.info("Training start")
+    # log_conf.logger.info("Training start")
 
     # 打开输入文件
     input_employer = codecs.open("./data_under_doing/employer_digital", 'r', encoding='utf-8')
@@ -117,8 +118,8 @@ def run_random_forest():
 
     count = 0
     # 读90个录取员工作为训练
-    lines = input_employer.readlines()
-    for line in lines:
+    employer_lines = input_employer.readlines()
+    for line in employer_lines:
         if count == 90:
             break
 
@@ -132,7 +133,8 @@ def run_random_forest():
 
     # 读100个落选者作为训练
     count = 0
-    for line in input_loser.readlines():
+    loser_lines = input_loser.readlines()
+    for line in loser_lines:
         if count == 100:
             break
 
@@ -154,7 +156,7 @@ def run_random_forest():
     clf = RandomForestClassifier(n_estimators=10)
     clf.fit(x, y)
 
-    log_conf.logger.info("Training finished.")
+    # log_conf.logger.info("Training finished.")
 
     # log_conf.logger.info("Creating image dot...")
 
@@ -163,28 +165,57 @@ def run_random_forest():
     #     f = tree.export_graphviz(clf, out_file=f)
 
     # 测试树的决策性能
-    log_conf.logger.info("Performance test starting...")
+    # log_conf.logger.info("Performance test starting...")
 
-    count = 0
+    TP = 0
+    FP = 0
+    true_bias = random.randint(0, 40)
+
+    TN = 0
+    FN = 0
+    false_bias = random.randint(0, 1000)
+
     test_count = 0
-    for line in lines:
-        if 90 <= count < 140:
+    for line in employer_lines:
+        if 90+true_bias <= count < 140+true_bias:
             person = json.loads(line)
             person_list = [person["7"], person["8"], person["9"], person["10"], person["11"], person["12"], person["13"],
                        person["15"], person["17"], person["23"]]
 
             if clf.predict(person_list)[0] == 1:
-                test_count += 1
+                TP += 1
+            else:
+                FN += 1
+            count += 1
 
         else:
-            if count > 140:
+            if count >= 140+true_bias:
                 break
             count += 1
-            continue
 
-        count += 1
+    count = 0
+    for line in loser_lines:
+        if 100+false_bias <= count < 200+false_bias:
+            person = json.loads(line)
+            person_list = [person["6"], person["7"], person["8"], person["9"], person["10"], person["11"], person["12"],
+                        person["14"], person["16"], person["22"]]
 
-    log_conf.logger.info("Accurate rate = "+str(test_count/50))
+            if clf.predict(person_list)[0] == 0:
+                TN += 1
+            else:
+                FP += 1
+            count += 1
+
+        else:
+            if count >= 1100 + true_bias:
+                break
+            count += 1
+
+    result = [TP, FP, TN, FN]
+    return result
+
+
+    # log_conf.logger.info("Accurate rate = "+str(test_count/50))
 
     # 关闭输入文件
     input_employer.close()
--- user_pos/test_main.py
+++ user_pos/test_main.py
@@ -1,12 +1,25 @@
 # -*- coding: utf-8 -*-
 
+from __future__ import division
 import algorithm
 
 # algorithm.run_decision_tree()
 
-sum = 0
+TP = 0
+FP = 0
+TN = 0
+FN = 0
 
-for index in range(0, 5000):
-    sum += algorithm.run_random_forest()
+for index in range(0, 500):
+    print index
+    result = algorithm.run_random_forest()
+    TP += result[0]
+    FP += result[1]
+    TN += result[2]
+    FN += result[3]
 
-print sum/5000
+precision = TP / (TP + FP)
+recall = TP / (TP + FN)
+accuracy = (TP + TN) / (TP+FP+TN+FN)
+
+print("precision = %f\nrecall = %f\naccuracy = %f" % (precision, recall, accuracy))
